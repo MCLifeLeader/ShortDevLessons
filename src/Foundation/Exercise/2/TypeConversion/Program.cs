912fe8f71f77a616d2e37022cbe3e1d2b02c7f46
Michael Carey <Michael@AGameEmpowerment.com>
2024-05-30 20:20:03 -0600

    chore: Update code for HelloWorld, UserInput, TypeConversion, and Variables exercises

--- src/Foundation/Exercise/2/TypeConversion/Program.cs
+++ src/Foundation/Exercise/2/TypeConversion/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int myInt = 1;
+        float myFloat = 1f;
+        bool myBoolean = true;
+        string myName = "John";
+        char myChar = 'a';
+        byte myByte = 0x20;
+        double myDouble = 1.75;
+
+        Console.WriteLine($"myInt = {myInt}");
+        Console.WriteLine($"myFloat = {myFloat}");
+        Console.WriteLine($"myBoolean = {myBoolean}");
+        Console.WriteLine($"myName = {myName}");
+        Console.WriteLine($"myChar = {myChar}");
+        Console.WriteLine($"myDouble = {myDouble}");
+
+        var x = 1;
+        var y = 2;
+        var sum = x + y;    // sum will also be defined as an integer
+
+        Console.WriteLine($"sum = {sum}");
+
+        var carType = CarType.Toyota;
+
+        Console.WriteLine($"carType = {carType}");
+    }
+
+    public enum CarType
+    {
+        Toyota = 1,
+        Honda = 2,
+        Ford = 3,
+    }
+}
